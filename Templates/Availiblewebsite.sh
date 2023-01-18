3a3ba8575e76a4e003ed8212cda27315260347f5
Nikitin Roman <78508681+IronWillDevops@users.noreply.github.com>
2023-01-18 03:55:21 +0200

    Update Availiblewebsite.sh

--- Templates/Availiblewebsite.sh
+++ Templates/Availiblewebsite.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-HTTP_STATUS="$(curl -s -IL $1 | grep HTTP  | awk {'print $2'})"
+HTTP_STATUS="$(curl -s -IL --max-time 5  $1 | grep HTTP  | awk {'print $2'})"
 count="$(echo $HTTP_STATUS |tr " " "\n" |wc -l)"
 result="$(echo $HTTP_STATUS | awk '{print $'$count'}')"
 echo "$result"
